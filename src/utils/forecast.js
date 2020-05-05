ec980c73a704f024b9d3237622e053bb0305af1d
pratikn1998 <pratiknallamotu@gmail.com>
2020-05-05 17:58:10 -0400

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,15 @@
+const request = require('request');
+const forecast = (latitude, longitutde, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=f6c68ee252cabced122a012237f0fbbc&query=' + encodeURIComponent(latitude) + ',' + encodeURIComponent(longitutde)  +  '?units=f';
+    request({url, json: true}, (error, { body }) => {
+        if(error) {
+            callback('Unable to connect to location services', undefined);
+        } else if (body.error) {
+            callback('Unable to find location', undefined);
+        } else {
+            callback(undefined, "The weather is " + body.current.weather_descriptions[0] + ".");
+        }
+    })
+};
+
+module.exports = forecast;
