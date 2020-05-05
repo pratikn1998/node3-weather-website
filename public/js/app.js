b084e6a7629a168f8da49a01e1832ffd9b0218e1
pratikn1998 <pratiknallamotu@gmail.com>
2020-05-05 18:24:53 -0400

    Setup App for Deployment

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     console.log("testing");
     console.log(location);
     messageOne.textContent = 'Loading Weather';
-    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    fetch('/weather?address=' + location).then((response) => {
 
         response.json().then((data) => {
             if(data.error) {
